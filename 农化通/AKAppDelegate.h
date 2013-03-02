7e48fe4fe18a5d2bad4a554ce45635f768de5199
凯_SKK <q254522834@kai-SKKtekiMac-mini.local>
2013-03-02 13:07:50 +0800

    Initial Commit

---
+++ 农化通/AKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AKAppDelegate.h
+//  农化通
+//
+//  Created by 凯_SKK on 13-3-2.
+//  Copyright (c) 2013年 山东乐世安通通信技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 农化通/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  农化通
+//
+//  Created by 凯_SKK on 13-3-2.
+//  Copyright (c) 2013年 山东乐世安通通信技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AKAppDelegate class]));
+    }
+}
